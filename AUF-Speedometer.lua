beb791f9fbb54ba41c224d7e8380173d62e632d4
FitterSpace <FitterSpace@gmail.com>
2021-04-18 15:56:25 -0500

    AuF-Speedometer.lua
    Added Health and the secret health bar you can get in Fire & Water. Watch aleckermit's "Agent Under Fire Secrets" video for more info on this interesting beta leftover.

--- AUF-Speedometer.lua
+++ AUF-Speedometer.lua
@@ -1,8 +1,9 @@
 ----- GLOBAL VARIABLES -----
-local Pointer = 0x803E74F4
+local SpeedPointer = 0x803E74F4
 local XAddress = 0x803E7504
 local YAddress = 0x803E7508
 local ZAddress = 0x803E750C
+local HealthPointer = 0x803F0108
 
 function onScriptStart()
 end
@@ -11,19 +12,28 @@ function onScriptCancel()
 end
 
 function onScriptUpdate()
-	local BaseAddress = GetPointerNormal(Pointer)
+	local BaseAddress = GetPointerNormal(SpeedPointer)
 	local OnFootSpeed = ReadValueFloat(BaseAddress + 0x410)
 	local FallingSpeed = ReadValueFloat(BaseAddress + 0x3F4)
 	local FaceButtons = ReadValue8(BaseAddress + 0x44C)
 	local XPosition = ReadValueFloat(XAddress)
 	local YPosition = ReadValueFloat(YAddress)
 	local ZPosition = ReadValueFloat(ZAddress)
+	local HealthAddress = GetPointerNormal(HealthPointer)
+	local Health = ReadValueFloat(HealthAddress + 0x290)
+	local SecretHealth = ReadValueFloat(HealthAddress + 0x294) --A beta leftover that adds another health bar for some reason. Watch aleckermit's AuF Secrets video for more info.
 
 	if FaceButtons > 0 then 
 		--WriteValueFloat(BaseAddress + 0x410, 200) --trying to make a moonjump cheat
 		ReleaseButton("Y")
 		PressButton("Y")
 		ReleaseButton("Y")
+	end
+
+	if SecretHealth > 0 then 
+		SecretHealthText = "\nExtra Health: " .. string.format("%.0f", SecretHealth)
+	else
+		SecretHealthText = ""
 	end
 
 	local text = "Speed: "
@@ -37,7 +47,10 @@ function onScriptUpdate()
 		"\nY: " ..
 		string.format("%.2f", YPosition) ..
 		"\nZ: " ..
-		string.format("%.2f", ZPosition)
+		string.format("%.2f", ZPosition) ..
+		"\nHealth: " ..
+		string.format("%.0f", Health) ..
+		SecretHealthText
 		)
 end
 
