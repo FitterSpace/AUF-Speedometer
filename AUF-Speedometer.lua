c16e6b3e931d49c2abeba6a3cba4246d31af16cf
FitterSpace <FitterSpace@gmail.com>
2021-04-18 14:51:06 -0500

    AuF-Speedometer.lua
    Added X, Y, and Z coordinates.

--- AUF-Speedometer.lua
+++ AUF-Speedometer.lua
@@ -1,5 +1,8 @@
 ----- GLOBAL VARIABLES -----
 local Pointer = 0x803E74F4
+local XAddress = 0x803E7504
+local YAddress = 0x803E7508
+local ZAddress = 0x803E750C
 
 function onScriptStart()
 end
@@ -12,6 +15,9 @@ function onScriptUpdate()
 	local OnFootSpeed = ReadValueFloat(BaseAddress + 0x410)
 	local FallingSpeed = ReadValueFloat(BaseAddress + 0x3F4)
 	local FaceButtons = ReadValue8(BaseAddress + 0x44C)
+	local XPosition = ReadValueFloat(XAddress)
+	local YPosition = ReadValueFloat(YAddress)
+	local ZPosition = ReadValueFloat(ZAddress)
 
 	if FaceButtons > 0 then 
 		--WriteValueFloat(BaseAddress + 0x410, 200) --trying to make a moonjump cheat
@@ -25,9 +31,13 @@ function onScriptUpdate()
 		text .. 
 		string.format("%.0f", OnFootSpeed) .. 
 		"\nFalling Speed: " .. 
-		string.format("%.0f", FallingSpeed) -- .. 
-		--"\nButton: " .. 
-		--FaceButtons
+		string.format("%.0f", FallingSpeed) ..
+		"\nX: " ..
+		string.format("%.2f", XPosition) ..
+		"\nY: " ..
+		string.format("%.2f", YPosition) ..
+		"\nZ: " ..
+		string.format("%.2f", ZPosition)
 		)
 end
 
